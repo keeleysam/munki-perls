6a665f4e39e84af4322c6f6b6143fc4ef380cea4
Samuel Keeley <samuel@keeley.net>
2026-07-27 20:35:40 -0500

    Fold latest_macos_supported and highest_supported_macos_version in too
    Same reasoning as the 17-file upgrade consolidation: both of these already used MunkiPerls::Upgrade and the same cached_hardware_snapshot() this file already calls. Leaving them as two separate files after that consolidation would have looked like an oversight rather than a decision, since they're squarely part of the same "Upgrade paths" family.
    
    t/contract.t asserts on output keys, not files, so it needed no changes for this one either.
    
    Signed-off-by: Samuel Keeley <samuel@keeley.net>

--- conditions/perls/upgrade_supported.pl
+++ conditions/perls/upgrade_supported.pl
@@ -1,13 +1,21 @@
 use 5.008006;
 use strict;
 use warnings;
-use MunkiPerls qw(perl_bool);
-use MunkiPerls::Upgrade qw(cached_hardware_snapshot evaluate_upgrade_perls);
+use MunkiPerls qw(perl_bool perl_string);
+use MunkiPerls::Upgrade qw(
+    cached_hardware_snapshot evaluate_upgrade_perls
+    highest_supported_macos_version latest_macos_supported
+);
 
 sub perls {
     my ($context) = @_;
     my $snapshot = cached_hardware_snapshot($context->{output_path});
     my $results = evaluate_upgrade_perls($snapshot);
-    return { map { $_ => perl_bool($results->{$_}) } keys %{$results} };
+    my %perls = map { $_ => perl_bool($results->{$_}) } keys %{$results};
+    $perls{latest_macos_supported} =
+        perl_bool(latest_macos_supported($snapshot));
+    $perls{highest_supported_macos_version} =
+        perl_string(highest_supported_macos_version($snapshot));
+    return \%perls;
 }
 1;
