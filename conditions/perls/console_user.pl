9cb50f0174be1e632e7d2294d7d5ceb552bcdbc3
Samuel Keeley <samuel@keeley.net>
2026-07-27 20:35:24 -0500

    Merge console_user_logged_in.pl into console_user.pl
    Both files called the exact same console_user_perls(), each destructuring a different half of its return value to report one of two related facts. Now one call, one file, two keys, same values as before. t/contract.t already asserts on the output keys, not files, so it needed no changes for this one.
    
    Signed-off-by: Samuel Keeley <samuel@keeley.net>

--- conditions/perls/console_user.pl
+++ conditions/perls/console_user.pl
@@ -1,10 +1,13 @@
 use 5.008006;
 use strict;
 use warnings;
-use MunkiPerls qw(perl_string);
+use MunkiPerls qw(perl_bool perl_string);
 use MunkiPerls::Perls qw(console_user_perls);
 sub perls {
-    my ($username) = console_user_perls();
-    return { console_user => perl_string($username) };
+    my ($username, $logged_in) = console_user_perls();
+    return {
+        console_user => perl_string($username),
+        console_user_logged_in => perl_bool($logged_in),
+    };
 }
 1;
